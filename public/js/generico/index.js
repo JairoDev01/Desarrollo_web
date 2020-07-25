2b6abc9c6365466f224ccc9c222a5380fadaf6c9
Jairo Velásquez <jairo.j5180@gmail.com>
2020-07-25 05:45:30 -0600

    Agregando boostrap

---
+++ public/js/generico/index.js
@@ -0,0 +1,97 @@
+function editar(modulo, opcion = "edit") {
+
+
+    let item = getItemSelected();
+
+
+    if (item == null) {
+        $('#errorToEdit').modal();
+    } else {
+        window.location.href = modulo + "/" + item + "/" + opcion;
+    }
+
+}
+
+function eliminar() {
+
+    item = getItemSelected();
+
+    if (item == null) {
+        $('#errorToEdit').modal();
+    } else {
+
+        $('#modal-delete-' + item).modal();
+    }
+
+}
+
+
+function getItemSelected() {
+
+    var items = document.getElementsByName('id_item');
+    var item = null;
+    var arrayItems = Object.keys(items).map(function (key) {
+        return [Number(key), items[key]];
+    });
+
+
+    arrayItems.forEach(function (element) {
+        if (element[1].checked) {
+            item = element[1].value;
+        }
+    });
+    return item;
+}
+
+
+function ver(modulo) {
+    let item = getItemSelected();
+
+    if (item == null) {
+        $('#errorToEdit').modal();
+    } else {
+        window.location.href = modulo + "/" + item + "";
+    }
+
+}
+
+function reporte(modulo) {
+    let item = getItemSelected();
+
+    if (item == null) {
+        $('#errorToEdit').modal();
+    } else {
+        window.location.href = modulo + "/" + item + "";
+    }
+
+}
+
+function darBaja(url) {
+
+    var CSRF_TOKEN = $('meta[name="csrf-token"]').attr('content');
+
+    $.ajax({
+
+        url: url,
+        type: 'post',
+        data: {_token: CSRF_TOKEN},
+        success: function (response) {
+
+            setTimeout(function () {
+                ajaxLoad(window.location.href);
+            }, 500);
+            $('.modal').modal('hide');
+
+
+
+        },
+        error: function (e) {
+            $('.modal').modal('hide');
+            alert(e);
+        }
+
+
+    });
+
+
+}
